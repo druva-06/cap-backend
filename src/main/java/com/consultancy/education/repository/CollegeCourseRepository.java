3bac4f90afc62e7bc5a071cf577970b35a521c77
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2025-01-12 19:36:27 +0530

    removed unused code

--- src/main/java/com/consultancy/education/repository/CollegeCourseRepository.java
+++ src/main/java/com/consultancy/education/repository/CollegeCourseRepository.java
@@ -1,6 +1,5 @@
 package com.consultancy.education.repository;
 
-import com.consultancy.education.DTOs.responseDTOs.collegeCourse.CollegeCourseResponseDto;
 import com.consultancy.education.model.CollegeCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
